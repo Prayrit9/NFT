9aa49bf58a606a6a8cc24b95cbea79652d2bb6e7
Prayrit9 <113416675+Prayrit9@users.noreply.github.com>
2023-08-23 22:14:26 +0530

    Create nft.js

---
+++ nft.js
@@ -0,0 +1,52 @@
+/*
+Assessment Requirements
+1. Create a variable that can hold a number of NFT's. What type of variable might this be?
+2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
+   The metadata values will be passed to the function as parameters. When the NFT is ready, 
+   you will store it in the variable you created in step 1
+3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
+4. For good measure, getTotalSupply() should return the number of NFT's you have created
+*/
+
+// create a variable to hold your NFT's
+const NFTs =[]
+
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (_name, _eyecolor, _shirtType, _bling) {
+    const NFT = {
+        "name" : _name,
+        "eyecolor": _eyecolor,
+        "shirtType": _shirtType,
+        "bling": _bling
+    }
+    NFTs.push(NFT);
+    console.log("Minted: " + _name);
+}
+
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
+function listNFTs () {
+for(let i= 0; i<NFTs.length; i++){
+    console.log("\n ID: \t\t"+ (i+1));
+    console.log("\n Name: \t\t"+NFTs[i].name);
+    console.log("\n Eyecolor: \t"+NFTs[i].eyecolor);
+    console.log("\n ShirtType: "+NFTs[i].shirtType);
+    console.log("\n Bling: \t"+NFTs[i].bling);
+}
+}
+
+// print the total number of NFTs we have minted to the console
+function getTotalSupply() {
+console.log("\n"+ NFTs.length);
+}
+
+// call your functions below this line
+mintNFT("Prayrit", "Black", "Tee", "Shades");
+mintNFT("Ishaan", "Black", "Sweater", "Bracelet");
+mintNFT("Ayaan", "Black", "Shirt", "Ring");
+mintNFT("Rohan", "Black", "Kurta", "Watch");
+mintNFT("Akash", "Black", "Hoodie", "Shades");
+listNFTs();
+getTotalSupply();
